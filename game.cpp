a68e6b7fb5fd4f61aa3c5b653729ba32654e7490
Mihail Denisov <denisov.mihail2013@yandex.ru>
2020-04-08 14:44:42 +0700

    Made window display

--- game.cpp
+++ game.cpp
@@ -1,8 +1,21 @@
 #include <SFML/Graphics.hpp>
-#include <time.h>
+
 using namespace sf;
 
 int main()
 {
-	
+	RenderWindow window( VideoMode(200, 200), "Test");
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}	
+
+		window.clear();
+		window.display();
+	}
+	return 0;
 } 
