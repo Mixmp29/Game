3e58c9b82e5bd13055f8fc29c0d9c6896f84eef4
Mihail Denisov <denisov.mihail2013@yandex.ru>
2020-04-10 16:42:57 +0700

    Add test movement

--- game.cpp
+++ game.cpp
@@ -1,10 +1,42 @@
 #include <SFML/Graphics.hpp>
 
 using namespace sf;
+/*
+const int H = 12;
+const int W = 40;
+
+String TileMap[H] = {
+
+"BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+"B                                 B    B",
+"B                                      B",
+"B                                      B",
+"B                                      B",
+"B                                      B",
+"B                                      B",
+"B                                      B",
+"B                                      B",
+"B                                      B",
+"B                                      B",
+"BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+
+};
+*/
 
 int main()
 {
-	RenderWindow window( VideoMode(200, 200), "Test");
+	RenderWindow window( VideoMode(800, 800), "Test");
+
+	CircleShape MyCircle(100.f);
+	MyCircle.setFillColor(Color::Green);
+
+	Texture t;
+	t.loadFromFile("pl.jpg");
+
+	Sprite s;
+	s.setTexture(t);
+	s.setPosition(100, 100); 
+
 	while (window.isOpen())
 	{
 		Event event;
@@ -12,9 +44,30 @@ int main()
 		{
 			if (event.type == Event::Closed)
 				window.close();
-		}	
+		}
+		
+		if(Keyboard::isKeyPressed(Keyboard::Left))
+		{
+			s.move(-0.1, 0);
+		}
+
+		if(Keyboard::isKeyPressed(Keyboard::Right))
+                {
+                        s.move(0.1, 0);
+                }
+		
+		if(Keyboard::isKeyPressed(Keyboard::Up))
+                {
+                        s.move(0, -0.1);
+                }
+		
+		if(Keyboard::isKeyPressed(Keyboard::Down))
+                {
+                        s.move(0, 0.1);
+                }
 
 		window.clear();
+		window.draw(s);
 		window.display();
 	}
 	return 0;
