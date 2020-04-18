63636afe711b6b7f2cb84342a1256b2621448af0
Mihail Denisov <denisov.mihail2013@yandex.ru>
2020-04-18 16:47:31 +0700

    Speed of movement of the character is tied to time

--- game.cpp
+++ game.cpp
@@ -42,9 +42,17 @@ int main() {
   s.setTextureRect( IntRect(0, 0, 50, 50) );
   s.setPosition(100, 100);
 
+  Clock clock;
+
   RectangleShape rectangle(Vector2f(32, 32));
 
   while (window.isOpen()) {
+
+    float time = clock.getElapsedTime().asMicroseconds();
+    clock.restart();
+
+    time = time/500;
+
     Event event;
     while (window.pollEvent(event)) {
       if (event.type == Event::Closed)
@@ -52,19 +60,19 @@ int main() {
     }
 
     if (Keyboard::isKeyPressed(Keyboard::Left)) {
-      s.move(-0.1, 0);
+      s.move(-0.1*time, 0);
     }
 
     if (Keyboard::isKeyPressed(Keyboard::Right)) {
-      s.move(0.1, 0);
+      s.move(0.1*time, 0);
     }
 
     if (Keyboard::isKeyPressed(Keyboard::Up)) {
-      s.move(0, -0.1);
+      s.move(0, -0.1*time);
     }
 
     if (Keyboard::isKeyPressed(Keyboard::Down)) {
-      s.move(0, 0.1);
+      s.move(0, 0.1*time);
     }
 
     window.clear();
